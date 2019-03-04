442a70d5870deb48bfd34d56fa392936b4a05a42
vic96418796 <vic96418796@gmail.com>
2019-03-04 22:14:57 +0800

    4849849

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.user.myapplication;
+
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.LocaleList;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(calcBMI);
+    }
+
+}
